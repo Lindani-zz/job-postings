a40cbce7a12768997274d016bae6ce2dd89a988c
Lindani <panilindani02@gmail.com>
2019-04-25 20:51:07 +0200

    add crud methods for creating and manipulating created jobs

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -2,16 +2,53 @@
 
 const Job = use('App/Models/Job')
 class JobController {
+    
     async home({view}){
-        // const job = new Job
-        // job.title = 'software'
-        // job.link = 'google.com'
-        // job.description = 'I want a job dude'
-
-        // await job.save()
-
         const jobs = await Job.all()
         return view.render('index', {jobs : jobs.toJSON()})
+    }
+
+    async userIndex ({view, auth}){
+        const jobs = await auth.user.jobs().fetch()
+        return view.render('jobs', {jobs : jobs.toJSON()})
+    }
+
+    async create ({request, response, auth, session}){
+        const job  = request.all()
+        const postedJob = await auth.user.jobs().create({
+            title: job.title,
+            link: job.link,
+            description: job.description,
+        })
+
+        session.flash({message: 'your job has been posted'})
+        return response.redirect('back')
+    }
+
+    async delete ({response, session, params}) {
+        const job = await Job.find(params.id)
+        await job.delete()
+
+        session.flash({message: 'your job has been removed'})
+        return response.redirect('back')
+    }
+
+    async edit({ params, view }) {
+        const job = await Job.find(params.id);
+        return view.render('edit', { job: job });
+    }
+
+    async update ({ response, request, session, params }) {
+        const job = await Job.find(params.id);
+
+        job.title = request.all().title;
+        job.link = request.all().link;
+        job.description = request.all().description;
+
+        await job.save();
+
+        session.flash({ message: 'Your job has been updated. '});
+        return response.redirect('/post-a-job');
     }
 }
 
